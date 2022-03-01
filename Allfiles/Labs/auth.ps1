4d4ff798fcecbadd6139781d8d7da0ba2e0e6077
Greger Martinussen <greger.martinussen@no.experis.com>
2022-03-01 09:35:07 +0100

    Hello, im signed

---
+++ Allfiles/Labs/auth.ps1
@@ -0,0 +1,9 @@
+﻿clientID 3826d202-ef1e-405c-906f-e893f2e5bf1c
+tennantID 4cfe372a-37a4-44f8-91b2-5faf34253c62
+4cfe372a-37a4-44f8-91b2-5faf34253c62
+
+aad_lab_user-20317034@LODSPRODMCA.onmicrosoft.com
+aad_lab_user2@gregersorg.onmicrosoft.com
+
+dotnet new mvc --auth SingleOrg --client-id <application_ID> --tenant-id <tenant_ID> --domain <domain_Name>
+dotnet dev-certs https --trust
